213a6bbe8bfd457654b8aafcf1aad0cb054f1ac9
ayoub <admin@devayb.com>
2025-08-13 09:47:10 +0300

    Remove "use client" from md-viewer component

--- components/md-viewer.tsx
+++ components/md-viewer.tsx
@@ -1,14 +1,17 @@
-"use client";
-
 import ReactMarkdown from "react-markdown";
+import Container from "./container";
 
 export default function PostContent({ post }) {
   console.log("post", post.content);
   return (
-    <div className="prose max-w-none prose-neutral dark:prose-invert rtl text-right">
-      <h1>{post.title}</h1>
-      <ReactMarkdown>{post.content}</ReactMarkdown>
-      <p className="mt-4 text-sm text-gray-500">By: {post.author.name}</p>
-    </div>
+    <Container>
+      <div className="prose text-right max-w-none prose-neutral dark:prose-invert rtl">
+        <h1 className="my-4 text-2xl font-medium text-center font-tajawal">
+          {post.title}
+        </h1>
+        <ReactMarkdown>{post.content}</ReactMarkdown>
+        <p className="mt-4 text-sm text-gray-500">By: {post.author.name}</p>
+      </div>
+    </Container>
   );
 }
