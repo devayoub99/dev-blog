4bd7a4a49ff0a0dac2487542171efbef208ef206
ayoub <admin@devayb.com>
2025-09-25 13:25:11 +0300

    TEMP: make the dashboard icon appear even for visitors

--- components/header.tsx
+++ components/header.tsx
@@ -41,27 +41,27 @@ export default async function Header() {
               </svg>
             </button>
 
-            {session?.user && (
-              <Link
-                href="/dashboard"
-                className="flex items-center h-full gap-2 p-2 border-gray-400 rounded-md cursor-pointer border-1"
+            {/* {session?.user && ( */}
+            <Link
+              href="/dashboard"
+              className="flex items-center h-full gap-2 p-2 border-gray-400 rounded-md cursor-pointer border-1"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 3h7v7H3V3zm11 0h7v7h-7V3zM3 14h7v7H3v-7zm11 0h7v7h-7v-7z"
-                  />
-                </svg>
-              </Link>
-            )}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 3h7v7H3V3zm11 0h7v7h-7V3zM3 14h7v7H3v-7zm11 0h7v7h-7v-7z"
+                />
+              </svg>
+            </Link>
+            {/* )} */}
           </div>
         </nav>
       </Container>
